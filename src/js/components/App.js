7220ccebd9919f72530895b5c55085619725bf15
loenko <loenko.js@gmail.com>
2018-02-22 20:33:35 +0500

    Prepush

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,13 @@
 const React = require('react');
 const AppActions = require('../actions/AppActions');
 const AppStore = require('../stores/AppStore');
+const SearchForm = require('./SearchForm');
 
 const App = React.createClass({
     render: function(){
         return(
             <div>
-                My App
+                <SearchForm />
             </div>
         )
     }
