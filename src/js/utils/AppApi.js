8c0a5684d94118743b6bb6959cd99f9c1c3e1938
loenko <loenko.js@gmail.com>
2018-02-25 00:05:19 +0500

    Changed http request to API to https

--- src/js/utils/AppApi.js
+++ src/js/utils/AppApi.js
@@ -1,6 +1,6 @@
 const AppActions = require('../actions/AppActions');
 const ApiKey = '10ff6c56';
-const ApiURL = `http://www.omdbapi.com/?apikey=${ApiKey}&s=`;
+const ApiURL = `https://www.omdbapi.com/?apikey=${ApiKey}&s=`;
 
 module.exports = {
     searchMovies: function(movie) {
