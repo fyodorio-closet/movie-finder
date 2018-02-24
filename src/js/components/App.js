3a7f906a421621692927a9fdd3871bf2c7adbf13
loenko <loenko.js@gmail.com>
2018-02-24 23:56:47 +0500

    Implemented search to console (via omdb API)

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,13 +3,32 @@ const AppActions = require('../actions/AppActions');
 const AppStore = require('../stores/AppStore');
 const SearchForm = require('./SearchForm');
 
+function getAppState() {
+    return {
+        movies: AppStore.getMovieResults()
+    }
+}
+
 const App = React.createClass({
+    getInitialState: function() {
+        return getAppState();
+    },
+    componentDidMount: function() {
+        AppStore.addChangeListener(this._onChange);
+    },
+    componentWillUnmount: function() {
+        AppStore.removeChangeListener(this._onChange);
+    },
     render: function(){
+        console.log(this.state.movies);
         return(
             <div>
                 <SearchForm />
             </div>
         )
+    },
+    _onChange: function() {
+        this.setState(getAppState());
     }
 });
 
--- src/js/utils/AppApi.js
+++ src/js/utils/AppApi.js
@@ -1,7 +1,19 @@
 const AppActions = require('../actions/AppActions');
+const ApiKey = '10ff6c56';
+const ApiURL = `http://www.omdbapi.com/?apikey=${ApiKey}&s=`;
 
 module.exports = {
     searchMovies: function(movie) {
-
+        $.ajax({
+            url: `${ApiURL}${movie.title}`,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                AppActions.receiveMovieResults(data.Search)
+            }.bind(this),
+            error: function(xhr, status, err) {
+                alert(err);
+            }.bind(this)
+        })
     }
 }
